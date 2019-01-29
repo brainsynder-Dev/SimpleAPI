5c07537599364a915e1fd343dd28df9aefb17360
brainsynder <briansnyder498@gmail.com>
2019-01-28 22:21:45 -0500

    Added "DataConverter" to help handle data that might change each version (Currently for items)

---
+++ v1_12_R1/src/main/java/simple/brainsynder/nms/v1_12_R1/DataConverterHandler.java
@@ -0,0 +1,28 @@
+package simple.brainsynder.nms.v1_12_R1;
+
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SpawnEggMeta;
+import simple.brainsynder.nms.DataConverter;
+
+public class DataConverterHandler extends DataConverter {
+    @Override
+    public EntityType getTypeFromItem(ItemStack stack) {
+        if (!stack.hasItemMeta()) return EntityType.UNKNOWN;
+        ItemMeta meta = stack.getItemMeta();
+        if (!(meta instanceof SpawnEggMeta)) return EntityType.UNKNOWN;
+        return ((SpawnEggMeta)meta).getSpawnedType();
+    }
+
+    @Override
+    public ItemStack getSpawnEgg(EntityType type, ItemStack stack) {
+        if (stack.getType() != Material.MONSTER_EGG) return stack;
+        if (!stack.hasItemMeta()) return stack;
+        SpawnEggMeta meta = (SpawnEggMeta) stack.getItemMeta();
+        meta.setSpawnedType(type);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+}
