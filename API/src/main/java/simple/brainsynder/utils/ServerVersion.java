e104c17aca46fa2d4ff9c7dc8e9a69dbbb1a079b
brainsynder <briansnyder498@gmail.com>
2019-02-05 13:39:55 -0500

    Added `ServerVersion.v1_14_R1`

--- API/src/main/java/simple/brainsynder/utils/ServerVersion.java
+++ API/src/main/java/simple/brainsynder/utils/ServerVersion.java
@@ -11,7 +11,8 @@ public enum ServerVersion {
     v1_11_R1 ("v1_11_R1"),
     v1_12_R1 ("v1_12_R1"),
     v1_13_R1 ("v1_13_R1"),
-    v1_13_R2 ("v1_13_R2");
+    v1_13_R2 ("v1_13_R2"),
+    v1_14_R1 ("v1_14_R1");
 
     private String version;
     ServerVersion (String version) {
