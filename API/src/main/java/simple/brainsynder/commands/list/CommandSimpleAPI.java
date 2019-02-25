90575bb967f3241fe010f3f55755c6a723070e81
brainsynder <briansnyder498@gmail.com>
2019-02-24 21:04:12 -0500

    Changed the '/simpleapi' command to the new Command System

--- API/src/main/java/simple/brainsynder/commands/list/CommandSimpleAPI.java
+++ API/src/main/java/simple/brainsynder/commands/list/CommandSimpleAPI.java
@@ -6,10 +6,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.scheduler.BukkitRunnable;
 import simple.brainsynder.Core;
-import simple.brainsynder.commands.BaseCommand;
-import simple.brainsynder.files.Language;
+import simple.brainsynder.commands.ParentCommand;
+import simple.brainsynder.commands.annotations.ICommand;
 import simple.brainsynder.nms.ITellraw;
-import simple.brainsynder.utils.Perms;
 import simple.brainsynder.utils.Reflection;
 import simple.brainsynder.utils.SpigotPluginHandler;
 
@@ -17,53 +16,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class CommandSimpleAPI extends BaseCommand {
-
-    public CommandSimpleAPI() {
-        super("simpleapi");
-    }
+@ICommand (
+        name = "simpleapi",
+        description = "Checks if any of the plugins that use SimpleAPI need updating"
+)
+public class CommandSimpleAPI extends ParentCommand {
 
     @Override
-    public void consoleSendCommandEvent(CommandSender sender, String[] args) {
+    public void run(CommandSender sender) {
         sender.sendMessage("§bSimpleAPI §9>> §7Looking for updates...");
-        try {
-            List<Boolean> updates = new ArrayList<>();
-            CompletableFuture.runAsync(() -> {
-                SpigotPluginHandler.updaterMap.forEach((plugin, handle) -> {
-                    if (handle.needsUpdate()) {
-                        String downloadURL;
-                        if (handle.getId() != 0) {
-                            downloadURL = "https://spigotmc.org/resources/" + handle.getId() + "/";
-                        } else {
-                            downloadURL = "https://spigotmc.org/";
-                        }
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                sender.sendMessage("§bSimpleAPI §9>> §7An update was found for " + handle.getPlugin().getDescription().getName() + "!");
-                                sender.sendMessage("§bSimpleAPI §9>> §7 Current Version: " + plugin.getDescription().getVersion());
-                                sender.sendMessage("§bSimpleAPI §9>> §7 New Version: " + handle.getVersion());
-                                sender.sendMessage("§bSimpleAPI §9>> §7 Download at: " + downloadURL);
-                                updates.add(true);
-                            }
-                        }.runTask(Core.getInstance());
-                    }
-                });
-
-
-            });
-        } catch (Exception e) {
-            sender.sendMessage("§bSimpleAPI §9>> §7All plugins using SimpleAPIs' updater are up to date");
-        }
-    }
-
-    @Override
-    public void playerSendCommandEvent(Player player, String[] args) {
-        if (!Perms.UPDATE.has(player)) {
-            player.sendMessage(Core.getLanguage().getString(Language.NOPERMISSION, true));
-            return;
-        }
-        player.sendMessage("§bSimpleAPI §9>> §7Looking for updates...");
         try {
             List<Boolean> updates = new ArrayList<>();
             SpigotPluginHandler.updaterMap.values().forEach(handler -> {
@@ -74,6 +35,7 @@ public class CommandSimpleAPI extends BaseCommand {
                 if (handler.getId() != 0) downloadURL = "https://spigotmc.org/resources/" + handler.getId() + "/";
                 tellraw.link(downloadURL);
 
+                String finalDownloadURL = downloadURL;
                 CompletableFuture.runAsync(() -> {
                     handler.needsUpdate((needsUpdate, version) -> {
                         tellraw.tooltip(
@@ -86,17 +48,30 @@ public class CommandSimpleAPI extends BaseCommand {
                             @Override
                             public void run() {
                                 updates.add(true);
-                                tellraw.send(player);
+                                if (sender instanceof Player) {
+                                    tellraw.send((Player) sender);
+                                } else {
+                                    sender.sendMessage("§bSimpleAPI §9>> §7An update was found for " + pdf.getName() + "!");
+                                    sender.sendMessage("§bSimpleAPI §9>> §7 Current Version: " + pdf.getVersion());
+                                    sender.sendMessage("§bSimpleAPI §9>> §7 New Version: " + version);
+                                    sender.sendMessage("§bSimpleAPI §9>> §7 Download at: " + finalDownloadURL);
+
+                                }
                             }
                         }.runTask(Core.getInstance());
                     });
                 });
             });
             if (updates.isEmpty()) {
-                player.sendMessage("§bSimpleAPI §9>> §7All plugins using SimpleAPIs' updater are up to date");
+                sender.sendMessage("§bSimpleAPI §9>> §7All plugins using SimpleAPIs' updater are up to date");
             }
         } catch (Exception e) {
-            player.sendMessage("§bSimpleAPI §9>> §7All plugins using SimpleAPIs' updater are up to date");
+            sender.sendMessage("§bSimpleAPI §9>> §7All plugins using SimpleAPIs' updater are up to date");
         }
+    }
+
+    @Override
+    public boolean canExecute(CommandSender sender) {
+        return sender.hasPermission("SimpleAPI.command.updater");
     }
 }
