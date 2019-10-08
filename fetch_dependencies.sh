6d80986c205d182b63ef26e1aaf5afb383e19f28
brainsynder <briansnyder498@gmail.com>
2019-10-08 19:51:35 -0400

    Fixed issue with getting Spigot/Craftbukkit jars

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -7,14 +7,14 @@ wget https://archive.mcmirror.io/CraftBukkit/craftbukkit-1.9.4-R0.1-SNAPSHOT.jar
 wget https://archive.mcmirror.io/CraftBukkit/craftbukkit-1.10.2-R0.1-SNAPSHOT.jar -P libs/
 wget https://archive.mcmirror.io/CraftBukkit/craftbukkit-1.11.2-R0.1-SNAPSHOT.jar -P libs/
 
-wget https://mcmirror.io/files/Spigot/Spigot-1.12-596221b_86aa17f-20170726-0522.jar -P libs/
-mv libs/Spigot-1.12-596221b_86aa17f-20170726-0522.jar libs/craftbukkit-1.12.2-R0.1-SNAPSHOT.jar
+wget https://archive.mcmirror.io/Spigot/spigot-1.12.2-R0.1-SNAPSHOT-b1648.jar -P libs/
+mv libs/spigot-1.12.2-R0.1-SNAPSHOT-b1648.jar libs/craftbukkit-1.12.2-R0.1-SNAPSHOT.jar
 
-wget https://mcmirror.io/files/Spigot/Spigot-1.13-fe3ab0d_f41aae4-20180815-2348.jar -P libs/
-mv libs/Spigot-1.13-fe3ab0d_f41aae4-20180815-2348.jar libs/craftbukkit-1.13-R0.1-SNAPSHOT.jar
+wget https://archive.mcmirror.io/Spigot/spigot-1.13-R0.1-SNAPSHOT-b1850.jar -P libs/
+mv libs/spigot-1.13-R0.1-SNAPSHOT-b1850.jar libs/craftbukkit-1.13-R0.1-SNAPSHOT.jar
 
-wget https://mcmirror.io/files/Spigot/Spigot-1.13.2-a1f2566-20181123-1027.jar -P libs/
-mv libs/Spigot-1.13.2-a1f2566-20181123-1027.jar libs/craftbukkit-1.13.1-R0.1-SNAPSHOT.jar
+wget https://archive.mcmirror.io/Spigot/spigot-1.13.2-R0.1-SNAPSHOT-b2147.jar -P libs/
+mv libs/spigot-1.13.2-R0.1-SNAPSHOT-b2147.jar libs/craftbukkit-1.13.1-R0.1-SNAPSHOT.jar
 
 wget https://mcmirror.io/files/Spigot/Spigot-1.14-0c02b0c-20190425-0538.jar -P libs/
 mv libs/Spigot-1.14-0c02b0c-20190425-0538.jar libs/craftbukkit-1.14-R0.1-SNAPSHOT.jar
