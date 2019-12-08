cb02b8c7f63f72caac88e37d7c8f19875aab2b58
brainsynder <briansnyder498@gmail.com>
2019-12-08 13:16:55 -0800

    Added v1_15_R1 ServerVersion

--- API/src/main/java/simple/brainsynder/utils/ServerVersion.java
+++ API/src/main/java/simple/brainsynder/utils/ServerVersion.java
@@ -4,17 +4,21 @@ import org.bukkit.Bukkit;
 
 public enum ServerVersion {
     UNKNOWN ("Unknown"),
-    v1_8_R3 ("v1_8_R3"),
-    v1_9_R1 ("v1_9_R1"),
-    v1_9_R2 ("v1_9_R2"),
-    v1_10_R1 ("v1_10_R1"),
-    v1_11_R1 ("v1_11_R1"),
-    v1_12_R1 ("v1_12_R1"),
-    v1_13_R1 ("v1_13_R1"),
-    v1_13_R2 ("v1_13_R2"),
-    v1_14_R1 ("v1_14_R1");
+    v1_8_R3,
+    v1_9_R1,
+    v1_9_R2,
+    v1_10_R1,
+    v1_11_R1,
+    v1_12_R1,
+    v1_13_R1,
+    v1_13_R2,
+    v1_14_R1,
+    v1_15_R1;
 
     private String version;
+    ServerVersion () {
+        this.version = name();
+    }
     ServerVersion (String version) {
         this.version = version;
     }
